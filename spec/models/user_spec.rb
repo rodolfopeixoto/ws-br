55aa50bd6384b1e5550af6bf17c55b039eb484fa
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-03-29 16:35:38 -0300

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should be_valid }
 
@@ -15,4 +16,19 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      expect(Devise).to receive(:friendly_token).and_return("authuniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql("authuniquetoken123")
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "authuniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql(existing_user.auth_token)
+    end
+  end
 end
